95407ec6348a949c86d3ad6107bf1617a73e96b5
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2021-07-20 13:11:20 -0300

    Update SistemaBots.py
    Adicionado print no método boas_vindas. Nova variável global "disponivel". Mudança no método escolhe_bot para verificar se o número escolhido está dentro das opções.

--- SistemaChatBot/SistemaBots.py
+++ SistemaChatBot/SistemaBots.py
@@ -1,6 +1,7 @@
 from Bots.Bot import *
 
 parar = False
+disponivel = True
 
 class SistemaChatBot:
     def __init__(self,nomeEmpresa,lista_bots):
@@ -14,7 +15,7 @@ class SistemaChatBot:
         self.__bot = None
     
     def boas_vindas(self):
-        return "Olá, esse é o sistema de chatbots da empresa " + self.__empresa
+        print("Olá, esse é o sistema de chatbots da empresa " + self.__empresa)
 
     def mostra_menu(self):
         print("Os chat bots disponíveis no momento são: ")
@@ -22,11 +23,14 @@ class SistemaChatBot:
             print(str(i) + " - " + "Bot: " + self.__lista_bots[i].nome + " - " + self.__lista_bots[i].apresentacao())
     
     def escolhe_bot(self):
+        global disponivel
         print("Digite o número do chat bot desejado: ")
         numero = int(input())
-        for i in range(len(self.__lista_bots)):
-            if numero == i:
-                self.__bot = self.__lista_bots[i]
+        if numero > len(self.__lista_bots)-1 or numero < 0:
+            print("Número indisponível")
+            disponivel = False
+        else:
+            self.__bot = self.__lista_bots[numero]
 
     def mostra_comandos_bot(self):
         for chave in self.__bot.mostra_comandos():
@@ -43,19 +47,22 @@ class SistemaChatBot:
 
     def inicio(self):
         global parar
+        global disponivel
         self.boas_vindas()
         print()
         self.mostra_menu()
         print()
         self.escolhe_bot()
         print()
-        self.__bot.boas_vindas()
-        while parar == False:
-            self.mostra_comandos_bot()
+        if disponivel == True:
+            print(self.__bot.boas_vindas())
             print()
-            self.le_envia_comando()
-            print()
-        print(self.__bot.despedida())
+            while parar == False:
+                self.mostra_comandos_bot()
+                print()
+                self.le_envia_comando()
+                print()
+            print(self.__bot.despedida())
 
 bot1 = BotSonolento("Soneca")
 bot2 = BotCuriosidade("Curioso")
